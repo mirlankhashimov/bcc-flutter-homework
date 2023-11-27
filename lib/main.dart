ee700986ee92eea08d38f0710927459cb0ac66a2
Mirlan <mirlan.khashimov>
2023-11-27 15:07:15 +0600

    bcc first homework taxi driver calculation

--- lib/main.dart
+++ lib/main.dart
@@ -67,9 +67,6 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
               '$_counter km',
               style: Theme.of(context).textTheme.headlineMedium,
